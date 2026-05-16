c9039788006d91e0fe853fb2e72218435a29db48
Muhammad Fakhr-Eddin <mfakhreddin@zad-solutions.com>
2026-05-16 11:50:48 +0300

    fix: add scanBasePackages and EnableScheduling to main application class

--- src/main/java/com/fakhr/notification_platform/NotificationPlatformApplication.java
+++ src/main/java/com/fakhr/notification_platform/NotificationPlatformApplication.java
@@ -2,8 +2,14 @@ package com.fakhr.notification_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.fakhr.notification")
+@EnableJpaRepositories(basePackages = "com.fakhr.notification.repository")
+@EntityScan(basePackages = "com.fakhr.notification.domain")
+@EnableScheduling
 public class NotificationPlatformApplication {
 
 	public static void main(String[] args) {
